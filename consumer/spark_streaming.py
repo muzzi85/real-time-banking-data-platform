4354a7b0c709f359fbd5d0c468b9711292eaced8
Dr. Mustafa Alhamdi <muzzi.alhamdi@gmail.com>
2026-05-19 00:49:22 +0100

    Added real-time fraud alert streaming pipeline

--- consumer/spark_streaming.py
+++ consumer/spark_streaming.py
@@ -1,9 +1,6 @@
 from pyspark.sql import SparkSession
-
 from pyspark.sql.functions import *
-
 from pyspark.sql.types import *
-
 
 spark = SparkSession.builder \
     .appName("BankingStreaming") \
@@ -17,30 +14,29 @@ spark = SparkSession.builder \
 schema = StructType([
 
     StructField("customer_id", IntegerType()),
-
     StructField("customer_name", StringType()),
-
     StructField("transaction_type", StringType()),
-
     StructField("amount", DoubleType()),
-
     StructField("country", StringType()),
-
     StructField("timestamp", DoubleType())
-])
 
+])
 
 df = spark.readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafka:29092") \
-    .option("subscribe", "banking-transactions") \
+    .option(
+        "kafka.bootstrap.servers",
+        "kafka:29092"
+    ) \
+    .option(
+        "subscribe",
+        "banking-transactions"
+    ) \
     .load()
-
 
 parsed_df = df.selectExpr(
     "CAST(value AS STRING)"
 )
-
 
 json_df = parsed_df.select(
     from_json(
@@ -49,16 +45,40 @@ json_df = parsed_df.select(
     ).alias("data")
 ).select("data.*")
 
-
-high_value_df = json_df.filter(
-    col("amount") > 1000
+fraud_df = json_df.filter(
+    col("amount") > 4000
+).withColumn(
+    "fraud_reason",
+    lit("HIGH_AMOUNT")
+).withColumn(
+    "risk_level",
+    lit("HIGH")
+).withColumn(
+    "alert_timestamp",
+    current_timestamp()
 )
 
+kafka_output_df = fraud_df.select(
+    to_json(
+        struct("*")
+    ).alias("value")
+)
 
-query = high_value_df.writeStream \
+query = kafka_output_df.writeStream \
     .outputMode("append") \
-    .format("console") \
+    .format("kafka") \
+    .option(
+        "kafka.bootstrap.servers",
+        "kafka:29092"
+    ) \
+    .option(
+        "topic",
+        "fraud-alerts"
+    ) \
+    .option(
+        "checkpointLocation",
+        "/tmp/fraud-checkpoint"
+    ) \
     .start()
-
 
 query.awaitTermination()
